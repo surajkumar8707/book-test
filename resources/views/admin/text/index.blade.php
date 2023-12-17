831934a2eaf0030e2830e6b9f271fc4346b7c1d2
Suraj Verma <suraj@nextolive.com>
2023-12-17 10:31:44 +0530

    new changes

--- resources/views/admin/text/index.blade.php
+++ resources/views/admin/text/index.blade.php
@@ -21,45 +21,47 @@
                                 @if (session()->has('success'))
                                     <div class="alert alert-success text-white success-delay-fadeout">{{ session('success') }}</div>
                                 @endif
-                                <table id="datatable" class="table table-bordered datatable">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Code</th>
-                                            <th>Name</th>
-                                            <th>Location</th>
-                                            <th>Category</th>
-                                            <th>Schedule</th>
-                                            <th>Price</th>
-                                            <th>Reported On</th>
-                                            <th>Created Date</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($texts as $tKey => $text)
+                                <div class="" style="overflow: auto;">
+                                    <table id="datatable" class="table table-bordered datatable">
+                                        <thead>
                                             <tr>
-                                                <td>{{ $text?->id }}</td>
-                                                <td>{{ $text?->code }}</td>
-                                                <td>{{ $text?->name }}</td>
-                                                <td>{{ $text?->location }}</td>
-                                                <td>{{ $text?->category }}</td>
-                                                <td>{{ $text?->schedule }}</td>
-                                                <td>{{ $text?->price }}</td>
-                                                <td>{{ $text?->reportedon }}</td>
-                                                <td>{{ date('d M Y h:i:s A', strtotime($text?->created_at)) }}</td>
-                                                <td>
-                                                    <a href="{{ route('admin.text.edit', base64_encode($text?->id)) }}" class="btn btn-sm btn-primary">
-                                                        <i class="fa fa-edit"></i>
-                                                    </a>
-                                                    <a href="{{ route('admin.text.delete', [base64_encode($text?->id)]) }}" class="delete-confirm btn btn-danger btn-sm ml-1">
-                                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                                    </a>
-                                                </td>
+                                                <th>ID</th>
+                                                <th>Code</th>
+                                                <th>Name</th>
+                                                <th>Location</th>
+                                                <th>Category</th>
+                                                <th>Schedule</th>
+                                                <th>Price</th>
+                                                <th>Reported On</th>
+                                                <th>Created Date</th>
+                                                <th>Action</th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($texts as $tKey => $text)
+                                                <tr>
+                                                    <td>{{ $text?->id }}</td>
+                                                    <td>{{ $text?->code }}</td>
+                                                    <td>{{ $text?->name }}</td>
+                                                    <td>{{ $text?->location }}</td>
+                                                    <td>{{ $text?->category }}</td>
+                                                    <td>{{ $text?->schedule }}</td>
+                                                    <td>{{ $text?->price }}</td>
+                                                    <td>{{ $text?->reportedon }}</td>
+                                                    <td>{{ date('d M Y h:i:s A', strtotime($text?->created_at)) }}</td>
+                                                    <td>
+                                                        <a href="{{ route('admin.text.edit', base64_encode($text?->id)) }}" class="btn btn-sm btn-primary">
+                                                            <i class="fa fa-edit"></i>
+                                                        </a>
+                                                        <a href="{{ route('admin.text.delete', [base64_encode($text?->id)]) }}" class="delete-confirm btn btn-danger btn-sm ml-1">
+                                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
--- resources/views/book-test/index.blade.php
+++ resources/views/book-test/index.blade.php
@@ -21,43 +21,45 @@
                                 @if (session()->has('success'))
                                     <div class="alert alert-success text-white success-delay-fadeout">{{ session('success') }}</div>
                                 @endif
-                                <table id="datatable" class="table table-bordered datatable">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Test name</th>
-                                            <th>Location</th>
-                                            <th>Price</th>
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Phone</th>
-                                            <th>Address</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($book_test as $tKey => $text)
+                                <div class="" style="overflow: auto;">
+                                    <table id="datatable" class="table table-bordered datatable">
+                                        <thead>
                                             <tr>
-                                                <td>{{ $text?->id }}</td>
-                                                <td>{{ $text?->text->name }}</td>
-                                                <td>{{ $text?->text->location }}</td>
-                                                <td>{{ $text?->text->price }}</td>
-                                                <td>{{ $text?->name }}</td>
-                                                <td>{{ $text?->email }}</td>
-                                                <td>{{ $text?->phone }}</td>
-                                                <td>{{ $text?->address }}</td>
-                                                <td>
-                                                    <a href="{{ route('admin.book-test.show', base64_encode($text?->id)) }}" class="btn btn-sm btn-primary">
-                                                        <i class="fa fa-eye"></i>
-                                                    </a>
-                                                    {{-- <a href="{{ route('admin.text.delete', [base64_encode($text?->id)]) }}" class="delete-confirm btn btn-danger btn-sm ml-1">
-                                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                                    </a> --}}
-                                                </td>
+                                                <th>ID</th>
+                                                <th>Test name</th>
+                                                <th>Location</th>
+                                                <th>Price</th>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Phone</th>
+                                                <th>Address</th>
+                                                <th>Action</th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($book_test as $tKey => $text)
+                                                <tr>
+                                                    <td>{{ $text?->id }}</td>
+                                                    <td>{{ $text?->text->name }}</td>
+                                                    <td>{{ $text?->text->location }}</td>
+                                                    <td>{{ $text?->text->price }}</td>
+                                                    <td>{{ $text?->name }}</td>
+                                                    <td>{{ $text?->email }}</td>
+                                                    <td>{{ $text?->phone }}</td>
+                                                    <td>{{ $text?->address }}</td>
+                                                    <td>
+                                                        <a href="{{ route('admin.book-test.show', base64_encode($text?->id)) }}" class="btn btn-sm btn-primary">
+                                                            <i class="fa fa-eye"></i>
+                                                        </a>
+                                                        {{-- <a href="{{ route('admin.text.delete', [base64_encode($text?->id)]) }}" class="delete-confirm btn btn-danger btn-sm ml-1">
+                                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                                        </a> --}}
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
